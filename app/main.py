d870afbc1b20cad996891b1d87354822b3393913
Ngnintedem3004 <ngnintedemmelong@gmail.com>
2026-02-17 07:21:03 +0100

    "Custom  Swagger"

--- app/main.py
+++ app/main.py
@@ -1,32 +1,38 @@
 from  fastapi import FastAPI
-
+from fastapi.openapi.docs import get_swagger_ui_html
 from app.routers import books
 
 app = FastAPI(
-    title="Library API",
-    description="API REST pour gérer une bibliothèque de livres",
-    version="1.0.0",
-    contact={
-        "name": "Support Technique",
-        "url": "https://support.example.com",
-        "email": "api-support@example.com"
-    },
-    license_info={
-        "name": "MIT License",
-        "url": "https://opensource.org/licenses/MIT"
-    },
-    openapi_tags=[                          # Tags pour organiser les routes
-        {
-            "name": "books",
-            "description": "Opérations sur les livres"
-        },
-        {
-            "name": "users",
-            "description": "Gestion des utilisateurs"
-        }
-    ]
+    doc_urls="none",
+    redoc_url="/none",
 )
 app.include_router(books.router)
 @app.get("/")
 def read_root():
     return {"message": "Bienvenue dans la bibliothèque API!"}
+@app.get("/docs",include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url="/openapi.json",
+        title="Librairy API Documentation",
+        swagger_favicon_url="https://fastapi.tiangolo.com/img/favicon.png",
+        swagger_ui_parameters={
+            "defaultModelsExpandDepth": -1 ,
+             "syntaxHighlight.theme":"monokai",
+             "tryItOutEnabled": True,
+            },
+    )
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = app.openapi(
+        title="Librairy API",
+        version="1.0.0",
+        description="API REST pour gérer une bibliothèque de livres.",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["title"] = "Librairy API"
+    openapi_schema["info"]["version"] = "1.0.0"
+    openapi_schema["info"]["description"] = "API pour gérer une bibliothèque de livres."
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
